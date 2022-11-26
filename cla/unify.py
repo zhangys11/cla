c819941ec3821b412a07dbc113e8f6eeda32d0c4
Yangxudong0 <117622613+Yangxudong0@users.noreply.github.com>
2022-11-26 19:38:55 +0800

    unify (#3)
    * Update metrics.py
    
    * Update metrics.py
    
    * Update metrics.py
    
    * Update metrics.py
    
    * Update unify.py

--- cla/unify.py
+++ cla/unify.py
@@ -21,17 +21,18 @@ from sklearn.linear_model import LinearRegression, LogisticRegression
 import pickle
 import joblib
 from datetime import datetime
+from sklearn.preprocessing import MinMaxScaler
 
 def analyze(X,y,use_filter=True,method='meta'):
     '''
     An include-all function that trains a meta-learner model of unified single metric.
-    And use that metric to evaluate the between-class and in-class classifiability.  
+    And use that metric to evaluate the between-class and in-class classifiability.
 
     Parameter
     ---------
     use_filter : whether to use R2 to filter highly correlated atom metrics
-    method : which method to use. 
-        'meta' - use linear regression as a meta-learner 
+    method : which method to use.
+        'meta' - use linear regression as a meta-learner
         'decompose' - decomposition, e.g., PCA
 
     Return
@@ -42,22 +43,43 @@ def analyze(X,y,use_filter=True,method='meta'):
     '''
     dic = calculate_atom_metrics(mu = X.mean(axis = 0), s = X.std(axis = 0),
                              mds = np.linspace(0, 6, 7+6*2),
-                             repeat = 5, nobs = 100, 
+                             repeat = 5, nobs = 100,
                              show_curve = True, show_html = True)
-    pkl_file = str(datetime.now()).replace(':','').replace('-','').replace(' ','') + '.pkl'                         
+    pkl_file = str(datetime.now()).replace(':','').replace('-','').replace(' ','') + '.pkl'
     joblib.dump(dic, pkl_file) # later we can reload with: dic = joblib.load('x.pkl')
-    
+
     _, keys, _, M = filter_metrics(dic, threshold = (0.5 if use_filter else None))
-    
     if method == 'decompose':
-        # M PCA decompose， PC1, PC2, PC3, % > PC1
-        # umetric_bw, umetric_in = ...
-        pass
-    else: # 'meta'
+        model, x_min, x_max = train_decomposer(M)
+        umetric_in = []
+        umetric_bw, umetric_in_ = calculate_unified_metric(X, y, model, keys, method)
+        if umetric_bw > x_max:
+            umetric_bw = x_max
+        elif umetric_bw < x_min:
+            umetric_bw = x_min
+        if umetric_in_[0] > x_max:
+            umetric_in_[0] = x_max
+        elif umetric_in_[0] < x_min:
+            umetric_in_[0] = x_min
+        if umetric_in_[1] > x_max:
+            umetric_in_[1] = 0.9 * x_max
+        elif umetric_in_[1] < x_min:
+            umetric_in_[1] = x_min
+        umetric_bw = abs((umetric_bw-x_max)/(x_min-x_max))
+        for i in umetric_in_:
+            z = (i-x_max)/(x_min-x_max)
+            z = abs(z)
+            umetric_in.append(z)
+
+    elif method == 'meta':
         model = train_metalearner(M, dic['d'])
-        umetric_bw, umetric_in = calculate_unified_metric(X, y, model, keys)
+        umetric_bw, umetric_in = calculate_unified_metric(X, y, model, keys, method)
+
+    else:
+        raise Exception('Unsupported method, must be meta or decompose')
 
     return umetric_bw, umetric_in, pkl_file
+    # return M,keys
 
 def mvgx(
     mu, # mean, row vector
@@ -68,7 +90,7 @@ def mvgx(
     '''
     Generate simulated high-dim (e.g., spectroscopic profiling) data
     This is an extended version of mvg() that accepts specified mu and s vectors.
-    
+
     Example
     -------
     X,y = mvgx(
@@ -77,28 +99,28 @@ def mvgx(
     nobs = 5, # number of observations / samples
     md = 1 # distance between means, respect to std, i.e. (mu2 - mu1) / std, or how many stds is the difference.
     )
-    '''    
-    
+    '''
+
     mu = np.array(mu)
     s = np.array(s)
- 
+
     N = nobs
     dims = len(mu)
     cov = np.diag(s**2)
-    
+
     mu1 = mu - s * md / 2
     mu2 = mu + s * md / 2
-    
+
     xc1 = np.random.multivariate_normal(mu1, cov, N)
     xc2 = np.random.multivariate_normal(mu2, cov, N)
-    
+
     y = np.concatenate((np.zeros(N), np.ones(N))).astype(int)
     X = np.vstack((xc1,xc2))
-        
+
     return X, y
 
-def calculate_atom_metrics(mu, s, mds, 
-repeat = 3, nobs = 100, 
+def calculate_atom_metrics(mu, s, mds,
+repeat = 3, nobs = 100,
 show_curve = True, show_html = True):
     '''
     Calculate atom metric values for different mds (between-group distances)
@@ -108,7 +130,7 @@ show_curve = True, show_html = True):
     mu : mean vector
     s : std vector
     mds : an array. between-classes mean distances (in std). e.g., np.linspace(0,1,10)
-    show_curve : whether output each metric curve against the between-class distance 
+    show_curve : whether output each metric curve against the between-class distance
     show_html : whether output an inline HTML table of metrics
 
     Example
@@ -118,20 +140,20 @@ show_curve = True, show_html = True):
                                  repeat = 1, nobs = 40)
     '''
 
-    dic = {}    
+    dic = {}
 
-    ## splits (divide 1 std into how many sections) * repeat         
+    ## splits (divide 1 std into how many sections) * repeat
     pbar = tqdm(total = len(mds) * repeat, position=0)
-    
-    ## only do visualization when repeat == 1 and draw == True 
+
+    ## only do visualization when repeat == 1 and draw == True
     #detailed = (repeat == 1 and draw == True)
 
     dic['d'] = np.array(mds)
 
     for md in mds:
-        
+
         raw_dic = {}
-        
+
         for i in range(repeat):
             X,y = mvgx(
                 mu = mu, # mean, row vector
@@ -142,34 +164,34 @@ show_curve = True, show_html = True):
 
             ## if detailed:
             #    print('d = ', round(md,3))
-            
+
             _, raw_dic1 = get_metrics(X,y)
             for k, v in raw_dic1.items():
                 if k in raw_dic:
                     raw_dic[k].append(v) # raw_dic[k] = raw_dic[k] + v
                 else:
                     raw_dic[k] = [v] # v
-            
+
             pbar.update()
             ## End of inner iteration ##
 
         for k, v in raw_dic.items():
 
-            trim_size = int(repeat / 10) 
+            trim_size = int(repeat / 10)
 
             if (repeat > 10): # remove the max and min
                 raw_dic[k] = np.mean(sorted(v)[trim_size:-trim_size])
             else:
                 raw_dic[k] = np.mean(v)
-            
+
         ## End of outer iteration ##
-    
+
         for k, v in raw_dic.items():
             if k in dic:
                 dic[k] = np.append(dic[k], v)
             else:
                 dic[k] = np.array([v])
-    
+
     if show_curve:
         print('visualize_dict()')
         visualize_dict(dic)
@@ -200,7 +222,7 @@ def filter_metrics(dic, threshold = 0.25, display = True):
 
     Note
     ----
-    For R2 effect size: 
+    For R2 effect size:
     Small: 0.01-0.09
     Medium: 0.09-0.25
     Large: 0.25 and higher
@@ -234,15 +256,39 @@ def filter_metrics(dic, threshold = 0.25, display = True):
 
     return dic_r2, keys, filtered_dic, np.array(M).T
 
+def train_decomposer(M):
+    umetric_in = []
+    dic2 = pd.DataFrame(M)
+    dic2.fillna(0, inplace=True)
+    dic2.replace(np.inf, 1, inplace=True)
+    dic2.replace(-np.inf, -1, inplace=True)
+    # dic2 = MinMaxScaler().fit_transform(dic2)
+
+    pca = PCA(n_components=3)
+    pca_model = pca.fit(dic2)
+    data1 = pca_model.transform(dic2)
+    x_min = min(data1.T[0])
+    x_max = max(data1.T[0])
+    # print(x_min, x_max)
+
+    a = pca.explained_variance_ratio_
+    PC1 = a[0]
+    PC2 = a[1]
+    PC3 = a[2]
+
+    print('The information interpretation rate of the first principal component is:', PC1)
+    return pca_model,x_min,x_max
+
+
 def train_metalearner(M, d, cutoff = 2):
     '''
     Train meta-learner using the atom metric matrix and the distance array.
 
     Parameter
     ---------
-    cutoff : use this cutoff to convert d to binary values, 
-    as we use logistic regression as the meta-learner output. 
-    default is 2, i.e., if md > 2, we deem it as fairly classifiable.  
+    cutoff : use this cutoff to convert d to binary values,
+    as we use logistic regression as the meta-learner output.
+    default is 2, i.e., if md > 2, we deem it as fairly classifiable.
     If you want more resolution in the lower range, choose a smaller cutoff.
 
     Note
@@ -251,27 +297,27 @@ def train_metalearner(M, d, cutoff = 2):
     In thee new version, we use logistic regression, to cover the between-class distance range (0-6std).
     For d = 6std means almost no overlap, the meta-learner will return 1
     '''
-    
+
     d = np.array(d) >= cutoff # np.median(d)
 
-    clf = LogisticRegression(max_iter=1000, solver='liblinear').fit(M, d) 
+    clf = LogisticRegression(max_iter=1000, solver='liblinear').fit(M, d)
     print('Score: ', clf.score(M, d))
     print('Coef and Intercept: ', clf.coef_, clf.intercept_)
     return clf
 
-def calculate_unified_metric(X, y, model, keys):
+def calculate_unified_metric(X, y, model, keys,method):
     '''
     First, fit a linear regression (meta-learner) model for M on d.
-    Then, calcualte the between-class and in-class unified metric on real dataset X and y. 
+    Then, calcualte the between-class and in-class unified metric on real dataset X and y.
 
     Parameters
     ----------
     model : meta-learner model, returned by train_metalearner()
     keys : selected metric names, returned by filter_metrics()
     '''
-    return AnalyzeBetweenClass(X ,y, model, keys), AnalyzeInClass(X, y, model, keys, repeat = 10)
+    return AnalyzeBetweenClass(X ,y, model, keys,method), AnalyzeInClass(X, y, model, keys,method)
 
-def AnalyzeBetweenClass(X, y, model, keys, method='meta'):
+def AnalyzeBetweenClass(X, y, model, keys, method='decompose'):
 
     X_pca = PCA(n_components = 2).fit_transform(X)
     plotComponents2D(X_pca, y)
@@ -281,19 +327,26 @@ def AnalyzeBetweenClass(X, y, model, keys, method='meta'):
 
     for key in keys:
         vec_metrics.append(new_dic[key])
-        
-    vec_metrics = np.nan_to_num(vec_metrics)
-    umetric = model.predict_proba([vec_metrics.T])[0]
-    print("between-class unified metric = ", umetric[1])
-    return umetric[1]
 
-def AnalyzeInClass(X, y, model, keys, repeat = 3, method='meta'):
+    vec_metrics = np.nan_to_num(vec_metrics)
+    if method == 'meta' and isinstance(model, LogisticRegression):
+        umetric = model.predict_proba([vec_metrics.T])[0][1]
+    elif method == 'decompose' and isinstance(model, PCA):
+        x=vec_metrics.reshape(1,-1)
+        # x = MinMaxScaler().fit_transform(x)
+        umetric = model.transform(x)[0][0]
+    else:
+        raise Exception('Unsupported method, must be meta or decompose')
+    # print("between-class unified metric = ", umetric[1])
+    return umetric
+
+def AnalyzeInClass(X, y, model, keys, method='decompose'):
 
     umetrics = []
-
+    repeat=10
     for c in set(y):
         Xc = X[y == c]
-        d = 0        
+        d = 0
 
         for i in range(repeat):
             yc = (np.random.rand(len(Xc)) > 0.5).astype(int) # random assign y labels
@@ -303,11 +356,12 @@ def AnalyzeInClass(X, y, model, keys, repeat = 3, method='meta'):
                 vec_metrics.append(new_dic[key])
 
             vec_metrics = np.nan_to_num(vec_metrics)
-            
+
             if method == 'meta' and isinstance(model, LogisticRegression):
-                d += model.predict_proba([vec_metrics.T])[0][1]           
+                d += model.predict_proba([vec_metrics.T])[0][1]
             elif method == 'decompose' and isinstance(model, PCA):
-                pass
+                x=vec_metrics.reshape(1,-1)
+                d += model.transform(x)[0][0]
             else:
                 raise Exception('Unsupported method, must be meta or decompose')
 
